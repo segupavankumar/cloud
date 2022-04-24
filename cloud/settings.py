8a3d3d92460b44f91a2a545d919660fed44c5c8f
segupavankumar <62418622+segupavankumar@users.noreply.github.com>
2022-04-24 20:37:00 +0530

    Update settings.py

--- cloud/settings.py
+++ cloud/settings.py
@@ -137,4 +137,4 @@ AWS_QUERYSTRING_AUTH = False
 # AWS_S3_FILE_OVERWRITE = False
 # AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
