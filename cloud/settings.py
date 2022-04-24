d503ee21979f2e9db9a8323b6d2cf334a59819e9
segupavankumar <62418622+segupavankumar@users.noreply.github.com>
2022-04-24 20:37:24 +0530

    Update settings.py

--- cloud/settings.py
+++ cloud/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r0l=_u&zepp5+d$nf3*y-0i!q0=ui7r51=x!s*uh3ejb=y&&!w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
